39e0fce0350b9ebbb1cae485ec138feee4e7240c
白各庄村的真名 <1179437831@qq.com>
2020-10-13 13:58:11 +0800

    这是一个测试用的主方法4

--- demo1/src/test/TestMain3.java
+++ demo1/src/test/TestMain3.java
@@ -5,5 +5,6 @@ import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
 public class TestMain3 {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hellow 4");
     }
 }
